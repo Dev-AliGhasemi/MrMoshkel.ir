c8a9cb7e9b7406d0cf0944fb9fb681e0b4465cf6
Dev-AliGhasemi <ali.ghasemeey@gmail.com>
2023-08-16 22:48:42 +0330

    Wed Aug 16 10:48:42 PM +0330 2023

---
+++ publish.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+startup.sh
+echo "--------------------------- Tomcat started ---------------------------"
+httrack  --update http://localhost:8080/home/posts -O /home/ali/Dropbox/Web/MrMoshkel/Publication/ +*localhost:8080* -n
+echo "--------------------------- Copy site done ---------------------------"
+if [ $? -eq 0 ]
+then
+	git commit -am "`date`" ; git push origin main
+	echo "--------------------------- Publish done ---------------------------"
+else
+	echo "publish failed."
+fi
+shutdown.sh
+echo "--------------------------- Tomcat shutdown ---------------------------"
