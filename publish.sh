bfe95c94b310e4340176adc6230e390b34e7cb5c
Dev-AliGhasemi <ali.ghasemeey@gmail.com>
2023-08-16 23:45:25 +0330

    Wed Aug 16 11:45:25 PM +0330 2023

--- publish.sh
+++ publish.sh
@@ -4,9 +4,9 @@ export JAVA_HOME="/usr/lib/jvm/jdk-17/bin/"
 
 cd /home/ali/Dropbox/Web/MrMoshkel/Publication/
 
-/home/ali/java/apache-tomcat-9.0.41/bin/startup.sh
+/home/ali/java/apache-tomcat-9.0.41/bin/startup.sh;
 echo "--------------------------- Tomcat started ---------------------------"
-httrack --update http://localhost:8080/home/posts -O /home/ali/Dropbox/Web/MrMoshkel/Publication/ +*localhost:8080* -n
+httrack --update http://localhost:8080/home/posts -O /home/ali/Dropbox/Web/MrMoshkel/Publication/ +*localhost:8080* -n;
 echo "--------------------------- Copy site done ---------------------------"
 if [ $? -eq 0 ]
 then
