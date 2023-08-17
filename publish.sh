5be76ffae908d66d2bcdc4d734d329d1dc359f94
Dev-AliGhasemi <ali.ghasemeey@gmail.com>
2023-08-17 06:53:17 +0330

    Thu Aug 17 06:53:17 AM +0330 2023

--- publish.sh
+++ publish.sh
@@ -1,20 +1,28 @@
 #!/bin/sh
 
-export JAVA_HOME="/usr/lib/jvm/jdk-17/bin/"
+export JAVA_HOME=/usr/lib/jvm/jdk-17/bin/;
+export CATALINA_HOME=/home/ali/java/apache-tomcat-9.0.41;
+export PATH=$PATH:$JAVA_HOME/bin:$CATALINA_HOME/bin;
 
-cd /home/ali/Dropbox/Web/MrMoshkel/Publication/
 
-/home/ali/java/apache-tomcat-9.0.41/bin/startup.sh
+cd /home/ali/Dropbox/Web/MrMoshkel/Publication/;
+
+startup.sh;
 sleep 4
-echo "--------------------------- Tomcat started ---------------------------"
-httrack --continue http://localhost:8080/home/posts -O /home/ali/Dropbox/Web/MrMoshkel/Publication/ +*localhost:8080* -n
-echo "--------------------------- Copy site done ---------------------------"
 if [ $? -eq 0 ]
 then
-	git add -A ; git commit -m "`date`" ; git push origin main
-	echo "--------------------------- Publish done ---------------------------"
-else
-	echo "publish failed."
-fi
-/home/ali/java/apache-tomcat-9.0.41/bin/shutdown.sh
+	echo "--------------------------- Tomcat started ---------------------------"
+	httrack --continue http://localhost:8080/home/posts -O /home/ali/Dropbox/Web/MrMoshkel/Publication/ +*localhost:8080* -n
+	echo "--------------------------- Copy site done ---------------------------"
+	if [ $? -eq 0 ]
+	then
+		git add -A ; git commit -m "`date`" ; git push origin main
+		echo "--------------------------- Publish done ---------------------------"
+	else
+		echo "publish failed."
+	fi
+shutdown.sh
 echo "--------------------------- Tomcat shutdown ---------------------------"
+else
+	echo "Problem in start Tomcat"
+fi
